e28204a119a2fd964f8f7f93789c207dabf88af1
Urie Guzman <Urie Guzman@UrielGZ>
2016-11-13 12:27:05 -0600

    Primera clase agregada para probar el Casting entre datos

---
+++ ProgramcionEjercicios/src/ejerciciosResueltos/CastingTest.java
@@ -0,0 +1,16 @@
+package ejerciciosResueltos;
+
+public class CastingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Un casting es una operación especial
+		 * que nos permite realizar una conversión entre diferentes tipos de datos en Java*/
+		int a=1;
+		double b=2.5;
+		a= (int) b;
+		System.out.println(a);
+
+	}
+
+}
